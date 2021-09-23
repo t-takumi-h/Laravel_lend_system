3377f342c2ea56faea9d9ddc19f4b87665d0fa1f
t-takumi-h <t.takumi.h@gmail.com>
2021-09-23 13:20:15 +0900

    TableControllerをリファクタリング
    コードの読みやすさの向上のために、TableControllerをリファクタリングしました。

--- app/Http/Controllers/TablesController.php
+++ app/Http/Controllers/TablesController.php
@@ -7,8 +7,10 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Table;
 use App\Http\Requests\Tables\CreateRequest;
 
+//Tableに関する制御（CRUD）の正常動作に責任をもつ
 class TablesController extends Controller
 {
+    //テーブルの一覧を表示する
     public function showTables()
     {
         $tables = Table::all();
@@ -16,18 +18,20 @@ class TablesController extends Controller
         return view('tables.tables')->with('tables', $tables);
     }
 
+    //テーブルの新規作成フォームを表示する
     public function showTableCreationForm()
     {
         return view('tables.table_creation_form');
     }
 
+    //テーブルを新規作成する
     public function createTable(CreateRequest $request)
     {
-        $user = Auth::user();
+        $user               = Auth::user();
+        $table              = new Table();
 
-        $table = new Table();
-        $table->title = $request->title;
-        $table->author_id = $user->id;
+        $table->title       = $request->title;
+        $table->author_id   = $user->id;
         $table->save();
 
         return redirect()->route('top');
